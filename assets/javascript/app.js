0824a61ca01e9f7e16f3c5bf6f561c8d58020ee8
Jerrica VanAlstyne <jsnow07@yahoo.com>
2019-10-04 13:29:06 -0400

    added css stylings and start button

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// PseudoCode
+// Create theme for trivia game--DONE
+// Create HTML and CSS styling ---DONE--
+// Game begins with start button displayed--DONE--
+// Once timer is clicked, the user has 30 seconds to select trivia answer
+    // Create an on-click funciton using jquery that slideshows to the first question and starts timeer (30 seconds)
+// Once a selection is made, the user answer is compared with the correct answer. 
+// If correct answer is made, add a point to the users score
+// If answer is incorrect, no addition is made. 
+// Either way, a gif of the correct answer is displayed for 5 seconds. 
+// Next question is displayed and the comparisons/score additions repeat. 
+// Game ends with a correct answers versus incorrect answers displayed. 
+// The game does not immediately start over, instead asks the user to start over. 
+
+
