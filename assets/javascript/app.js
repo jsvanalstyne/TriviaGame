d2efdd49cf7b75eae02395aec9f6f3210713f99c
Jerrica VanAlstyne <jsnow07@yahoo.com>
2019-10-04 14:42:28 -0400

    added a framework for questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,12 +4,110 @@
 // Game begins with start button displayed--DONE--
 // Once timer is clicked, the user has 30 seconds to select trivia answer
     // Create an on-click funciton using jquery that slideshows to the first question and starts timeer (30 seconds)
+    var tarheelQuestions = [
+        {questions: "tarheel question 1",
+        answer: {
+            a: "answer a",
+            b: "answer b",
+            c: "answer c",
+            d: "answer d"
+        },
+        correctAnswer: "answer c"
+        },
+        {questions: "tarheel question 1",
+        answer: {
+            a: "answer a",
+            b: "answer b",
+            c: "answer c",
+            d: "answer d"
+        },
+        correctAnswer: "answer a "
+        },
+        {questions: "tarheel question 1",
+        answer: {
+            a: "answer a",
+            b: "answer b",
+            c: "answer c",
+            d: "answer d"
+        },
+        correctAnswer: "answer d"
+        },
+        {questions: "tarheel question 1",
+        answer: {
+            a: "answe ra",
+            b: "answer b",
+            c: "answer c",
+            d: "answer d"
+        },
+        correctAnswer: "answer c"
+        },
+        {questions: "tarheel question 1",
+        answer: {
+            a: "answer a",
+            b: "answer b",
+            c: "answer c",
+            d: "answer d"
+        },
+        correctAnswer: "answer b"
+        },
+        {questions: "tarheel question 1",
+        answer: {
+            a: "answer a",
+            b: "answer b",
+            c: "answer c",
+            d: "answer d"
+        },
+        correctAnswer: "answer a"
+        },
+        {questions: "tarheel question 1",
+        answer: {
+            a: "answer a",
+            b: "answer b",
+            c: "answer c",
+            d: "answer d"
+        },
+        correctAnswer: "answer a"
+        },
+        {questions: "tarheel question 1",
+        answer: {
+            a: "answer a",
+            b: "answer b",
+            c: "answer c",
+            d:"answer d"
+        },
+        correctAnswer: "answer b"
+        },
+        {questions: "tarheel question 1",
+        answer: {
+            a: "answer a",
+            b: "answer b",
+            c: "answer c",
+            d: "answer d"
+
+        },
+        correctAnswer: "answer c"
+        },
+        {questions: "tarheel question 1",
+        answer: {
+            a: "answer a",
+            b: "answer b",
+            c: "answer c",
+            d: "answer d"
+        },
+        correctAnswer: "answer d"
+        },
+    ];
+    $(document).ready(function() { 
+     $("#start").click(function(){
+
+     });
+    // Create an array of object questions for the user to answer one at a time. 
 // Once a selection is made, the user answer is compared with the correct answer. 
-// If correct answer is made, add a point to the users score
-// If answer is incorrect, no addition is made. 
-// Either way, a gif of the correct answer is displayed for 5 seconds. 
+// If correct answer is made, add a point to the correct variable
+// If answer is incorrect, add a point to the inccorect variable
+// Either way, a gif of the correct answer is displayed for 5 seconds.--Think Slideshow--
 // Next question is displayed and the comparisons/score additions repeat. 
 // Game ends with a correct answers versus incorrect answers displayed. 
 // The game does not immediately start over, instead asks the user to start over. 
 
-
+    });
