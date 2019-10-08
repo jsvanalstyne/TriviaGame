16c90b041ce3bee45d95aeda8b1af00469c67496
Jerrica VanAlstyne <jsnow07@yahoo.com>
2019-10-08 18:19:25 -0400

    fixed comparison statements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,9 @@
     var incorrectSelection=0;
     var questionIndex=0;
     var time= 15000;
-    var userClick="";
+    var userClck;
+    var correctAnswer;
+   
     
     // Create an array of object questions for the user to answer one at a time.
     var friendsQuestions = [
@@ -73,18 +75,24 @@
 
         
         for(var i=0; i<friendsQuestions[questionIndex].answer.length; i++){
-            $(".answerChoices").prepend("<button><p>" +friendsQuestions[questionIndex].answer[i]+ "</button></p>");
+            $(".answerChoices").append("<button><p>" +friendsQuestions[questionIndex].answer[i]+ "</button></p>");
             // $(".answerChoices").html("<br>");
             console.log("answerdisplayed");
+            // $("#answerChoice0").text(friendsQuestions[questionIndex].answer[0]);
+            // $("#answerChoice1").text(friendsQuestions[questionIndex].answer[1]);
+            // $("#answerChoice2").text(friendsQuestions[questionIndex].answer[2]);
+            // $("#answerChoice3").text(friendsQuestions[questionIndex].answer[3]);
+
         }
         ;}
+   
     
                    
      function userWin(){
          $("#gameOn").append("OH. MY. GAWWDDD! You got it correct!!");
          correctSelection ++;
-         userClick = friendsQuestions[questionIndex].correctAnswer;
-         $("#gameOn").append("The correct answer was " + correctAnswer + friendsQuestions[questionIndex].image);
+        //  userClick = friendsQuestions[questionIndex].correctAnswer;
+         $("#image-holder").append("The correct answer was " + friendsQuestions[questionIndex].correctAnswer + friendsQuestions[questionIndex].image);
          setTimeout(nextQuestion, 5000);
          questionIndex ++; 
      }   
@@ -92,7 +100,8 @@
      function userLoss(){
          $("#gameOn").append("Could you be anymore wrong?")
          incorrectSelection ++;
-         $("#gameOn").append("The correct answer was " + correctAnswer + friendsQuestions[questionIndex].image);
+         $("#gameOn").append("The correct answer was " + friendsQuestions[questionIndex].correctAnswer + friendsQuestions[questionIndex].image);
+         
          setTimeout(nextQuestion, 5000);
          questionIndex ++;
      }
@@ -100,7 +109,7 @@
          if(time===0){
              $("#gameOn").append("You weren't there for me!")
              incorrectSelection ++;
-             $("#gameOn").append("The correct answer was " + correctAnswer + friendsQuestions[questionIndex].image);
+             $("#gameOn").append("The correct answer was " + friendsQuestions[questionIndex].correctAnswer + friendsQuestions[questionIndex].image);
              setTimeout(nextQuestion, 5000);
              questionIndex++;
          }
@@ -118,11 +127,12 @@
      function timer(){
         intervalId = setInterval(decrement, 15000);
      }
-     function restartGame(){
-        $("#gameOn").html("<p>Correct answers: "+ correctSelection +"</p><p>Incorrect answers: "+ incorrectSelection +"</p>")
-        $("#start").show();
-     }
-     restartGame;
+    //  function restartGame(){
+    //     $("#gameOn").html("<p>Correct answers: "+ correctSelection +"</p><p>Incorrect answers: "+ incorrectSelection +"</p>")
+    //     $("#gameOn").html("<p> Press Start to play")
+    //     $("#start").show();
+    //  }
+    //  restartGame;
            
        
         
@@ -132,22 +142,31 @@
          console.log("click");
          $("#start").hide();
          asking(); 
-         setTimeout(nextQuestion, 5000);   
+        //  timer();
+        //  setTimeout(nextQuestion, 5000);   
                
      
-     });
-     $(".button").on("click", function(){
-         userClick = event.click;
-         if(userClick = friendsQuestions[questionIndex].correctAnswer){
-             userWin();
-         }
-         else if ( time===0){
-             outOfTime();
-         }
-         else{
-             userLoss();
-         }       
+     
+        
      })
+     $(".answerChoices").click(function(){
+        userClick= $(this).val();
+        console.log("buttonclicked");
+        console.log(this);
+     
+        if(userClick === friendsQuestions[questionIndex].correctAnswer){
+            console.log(friendsQuestions[questionIndex].correctAnswer);
+            userWin();
+        }
+        else if ( time===0){
+            outOfTime();
+        }
+        else{
+            userLoss();
+        }    
+        nextQuestion();
+    });
+    
      
 // Once a selection is made, the user answer is compared with the correct answer. 
 // If correct answer is made, add a point to the correct variable
@@ -156,5 +175,5 @@
 // Next question is displayed and the comparisons/score additions repeat. 
 // Game ends with a correct answers versus incorrect answers displayed. 
 // The game does not immediately start over, instead asks the user to start over. 
-
-    });
+});
+ 
